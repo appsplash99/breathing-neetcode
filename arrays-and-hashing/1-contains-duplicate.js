a9972174a4acddd5d0e32c2a003acc0f7c91d0f3
apurvc99 <apurv@venwiz.com.com>
2022-06-20 09:42:47 +0530

    chore: 💪 day#1

---
+++ arrays-and-hashing/1-contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * 217. Contains Duplicate
+ * https://leetcode.com/problems/contains-duplicate/
+ */
+
+let containsDuplicate = function(nums) {
+  const dupes = {};
+  for (let i = 0; i<nums.length; i++) {
+    if (dupes[nums[i]]) {
+      return true;
+    } else {
+      dupes[nums[i]] = true;
+    }
+  }
+  return false;
+};
