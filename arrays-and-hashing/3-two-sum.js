8ba13c678add9dc5ccc27c139afad612e421cc37
apurvc99 <apurv@venwiz.com.com>
2022-06-22 21:55:50 +0530

    chore: 💪 day#3

---
+++ arrays-and-hashing/3-two-sum.js
@@ -0,0 +1,41 @@
+/**
+ * 1. Two Sum
+ * https://leetcode.com/problems/two-sum/
+ */
+
+/**
+ * Time Complexity - O(n)
+ * Space complexity - O(n)
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+let twoSum = function (nums, target) {
+  const numsObj = {};
+  let indices = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = numsObj[target - nums[i]];
+    if (diff !== undefined) {
+      indices = diff > i ? [i, diff] : [diff, i];
+    } else {
+      numsObj[nums[i]] = i;
+    }
+  }
+  return indices;
+};
+
+/** --------- */
+
+const TEST_CASES = [
+  { nums: [2, 7, 11, 15], target: 9, op: [0, 1] },
+  { nums: [3, 2, 4], target: 6, op: [1, 2] },
+  { nums: [3, 3], target: 6, op: [0, 1] },
+  { nums: [1, 3, 4, 2], target: 6, op: [2, 3] },
+];
+
+TEST_CASES.forEach((test) => {
+  console.log(test.op, twoSum(test.nums, test.target));
+});
